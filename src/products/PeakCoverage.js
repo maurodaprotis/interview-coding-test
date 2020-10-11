2e4386a1058191fab12be88ef9bb851e29331fd4
Mauro Daprotis <maurodaprotis@gmail.com>
2020-10-10 21:09:55 -0300

    :fire: Removing duplicate code on Peak Coverage

--- src/products/PeakCoverage.js
+++ src/products/PeakCoverage.js
@@ -10,17 +10,6 @@ class PeakCoverage extends Product {
     super(`Peak Coverage`, sellIn, price);
   }
 
-  set price(p) {
-    this._price =
-      p > Product.MAX_PRICE
-        ? Product.MAX_PRICE
-        : Math.max(p, Product.MIN_PRICE);
-  }
-
-  get price() {
-    return this._price;
-  }
-
   updatePrice() {
     this._substractOneDay();
     this.price = this._isOnSale() ? this.price + 1 : this.price - 1;
